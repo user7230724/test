d5cb7b997a2e34376e22f386d8fe8f21f8b0c60f
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-12-20 06:14:49 +0100

    commit

---
+++ info/test.hs
@@ -0,0 +1,29 @@
+import qualified Debug.Trace as Trace
+
+infixr 0 #
+(#) = id
+
+type Message = [String]
+
+newtype Msg = Msg Message
+
+instance Show Msg where
+  show (Msg msg) = concat msg
+
+nop :: (Monad m) => m ()
+nop = pure ()
+
+trace' :: (Show a) => a -> b -> b
+trace' = Trace.traceShow
+
+tracem :: (Monad m, Show a) => a -> m ()
+tracem a = do
+  () <- trace' a nop
+  nop
+
+main :: IO ()
+main = do
+  tracem # Msg ["∃"]
+  putStrLn "x"
+  tracem # Msg ["∃"]
+  putStrLn "y"
